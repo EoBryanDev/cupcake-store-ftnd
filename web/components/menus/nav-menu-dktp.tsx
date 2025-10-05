18a597902a5043970f7ac551e295c606f810a3c8
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-05 10:37:13 -0300

    Part of EOB-12,EOB-34: feat: creating menu bar

---
+++ web/components/menus/nav-menu-dktp.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ThemeToggle } from "@/components/buttons/theme-toggle";
+import { NotepadText, Menu } from "lucide-react";
+
+import Link from "next/link";
+import { SliderMenu } from "./slider-menu";
+
+export function NavMenuDesktop() {
+  return (
+    <header className="bg-card border-b">
+      <div className="flex items-center justify-between px-4 py-4 sm:px-6">
+        <div className="flex items-center space-x-2 sm:space-x-8">
+          <h1 className="text-xl font-bold sm:text-2xl">
+            <Link href="/v1/dashboard" className="cursor-pointer">
+              CK
+            </Link>
+          </h1>
+        </div>
+
+        <div className="flex items-center space-x-2 sm:space-x-8">
+          <nav className="hidden items-center space-x-6 sm:flex">
+            <div className="hover:text-primary flex items-center space-x-2">
+              <Link href="/" className="cursor-pointer text-sm">
+                Home
+              </Link>
+            </div>
+            <div className="hover:text-primary flex items-center space-x-2">
+              <Link href="/" className="cursor-pointer text-sm">
+                Products
+              </Link>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Link
+                href="/"
+                className="hover:text-primary cursor-pointer text-sm"
+              >
+                Categories
+              </Link>
+            </div>
+          </nav>
+        </div>
+
+        <div className="flex items-center space-x-2 sm:space-x-4">
+          <NotepadText className="h-5 w-5 cursor-pointer" />
+          <ThemeToggle />
+          <SliderMenu />
+        </div>
+      </div>
+    </header>
+  );
+}
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { NavMenu } from "@/components/menus/nav-menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NavMenu />
           {children}
           <Toaster />
         </ThemeProvider>
