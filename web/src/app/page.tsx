61ef6b86db8be1acd57aa4dcb074d5c583ca2dac
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-05 11:20:07 -0300

    Part of EOB-12,EOB-42: feat: creating footer component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
+import { MainFooter } from "@/components/footers/main-footer";
+import { NavMenu } from "@/components/menus/nav-menu";
 import { HomePage } from "@/components/pages/home-page";
 
 export default function Home() {
   return (
     <>
+      <NavMenu />
       <HomePage />
+      <MainFooter />
     </>
   );
 }
