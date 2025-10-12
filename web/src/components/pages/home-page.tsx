05aa49081c0bebf888a3aead02e1bb44eb9efe19
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-12 15:40:24 -0300

    Part of EOB-12,EOB-39: feat: listing products

--- web/src/components/pages/home-page.tsx
+++ web/src/components/pages/home-page.tsx
@@ -1,11 +1,26 @@
+"use client";
+import { useProductVariantQuery } from "@/src/hooks/queries/useProductVariants";
 import { MainContainer } from "../containers/main-container";
 import { ProductList } from "../products/product-list";
 import { Section } from "../sections/section";
 import { Section85 } from "../sections/section85";
 import { HighLightTitle } from "../sections/titles/highlight-title";
 import { Subtitle } from "../sections/titles/subtitle";
+import { IProductData } from "@/src/interface/IProductVariant";
+import { PromoBanner } from "../banners/promo-banner";
 
-function HomePage() {
+interface IHomePage {
+  initialNwstVariantProd: IProductData | null;
+}
+
+function HomePage({ initialNwstVariantProd }: IHomePage) {
+  const {
+    data: newestProducts,
+    isLoading,
+    error,
+  } = useProductVariantQuery("newest", {
+    initialData: initialNwstVariantProd,
+  });
   return (
     <>
       <MainContainer>
@@ -28,7 +43,14 @@ function HomePage() {
         <Section85>
           <HighLightTitle>Newest Products</HighLightTitle>
           <Subtitle>Our newest products that will impress your tasty</Subtitle>
-          <ProductList products={} />
+          {isLoading && <div>Loading...</div>}
+          {error && <div>Error loading products</div>}
+          <div className="mt-8 py-4">
+            {newestProducts && <ProductList products={newestProducts.data} />}
+          </div>
+        </Section85>
+        <Section85>
+          <PromoBanner src="https://pub-3487eb3e73174ed99e160777dbdb7a0f.r2.dev/promo-halloween.png"></PromoBanner>
         </Section85>
       </MainContainer>
     </>
---
+++ web/src/components/products/product-item.tsx
@@ -0,0 +1,52 @@
+import { formatCentsToBRL } from "@/src/helpers/money";
+import { IProduct } from "@/src/interface/IProductVariant";
+import { cn } from "@/src/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+interface IProductItemProps {
+  product: IProduct;
+  textContainerClassName?: string;
+}
+
+const ProductItem = ({
+  product,
+  textContainerClassName,
+}: IProductItemProps) => {
+  const firstVariant = product.variants[0];
+
+  return (
+    <Link
+      href={`/product-variant/${firstVariant.slug}`}
+      className="flex flex-col gap-4"
+    >
+      <Image
+        src={
+          firstVariant.imageUrl ??
+          "https://pub-3487eb3e73174ed99e160777dbdb7a0f.r2.dev/cupcake_halloween.png"
+        }
+        alt={firstVariant.name}
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-auto min-h-[300px] w-auto min-w-[300px] rounded-3xl object-cover"
+      />
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
+        <p className="truncate text-sm font-medium">{product.name}</p>
+        <p className="text-muted-foreground truncate text-xs font-medium">
+          {product.description}
+        </p>
+        <p className="truncate text-sm font-semibold">
+          {formatCentsToBRL(firstVariant.priceInCents)}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItem;
