67b47875457218b2ab1ee87f22307292c40a0686
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-12 15:38:45 -0300

    Part of EOB-12,EOB-39: chore: adding r2 config

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-3487eb3e73174ed99e160777dbdb7a0f.r2.dev",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
