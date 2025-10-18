751fde002140f248b2925cf6c0b35830a20bf832
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-17 22:27:36 -0300

    fix: trying fixing to deploy

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,22 @@
+// page.tsx
 import { MainFooter } from "@/src/components/footers/main-footer";
 import { NavMenu } from "@/src/components/menus/nav-menu";
 import { HomePage } from "@/src/components/pages/home-page";
 import { getProductVariant } from "../services/product-variant.service";
 
 export default async function Home() {
-  const newestProdVarResp = await getProductVariant("newest");
-  const newestProdVarRespData = newestProdVarResp?.data || null;
+  let newestProdVarRespData = null;
+  let popularProdVarRespData = null;
 
-  const popularProdVarResp = await getProductVariant(null);
-  const popularProdVarRespData = popularProdVarResp?.data || null;
+  // Só faz fetch se NÃO estiver em build time
+  if (process.env.NODE_ENV !== "production" || typeof window !== "undefined") {
+    const newestProdVarResp = await getProductVariant("newest");
+    newestProdVarRespData = newestProdVarResp?.data || null;
+
+    const popularProdVarResp = await getProductVariant(null);
+    popularProdVarRespData = popularProdVarResp?.data || null;
+  }
+
   return (
     <div className="flex min-h-screen max-w-screen flex-col">
       <NavMenu />
