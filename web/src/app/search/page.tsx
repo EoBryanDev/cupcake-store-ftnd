4c581516577754be476cbecffcbeae431ce37be7
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-10-24 13:07:54 -0300

    Part of EOB-23,EOB-54: feat: creating search page

---
+++ web/src/app/search/page.tsx
@@ -0,0 +1,16 @@
+import { MainFooter } from "@/src/components/footers/main-footer";
+import { NavMenu } from "@/src/components/menus/nav-menu";
+import { ProductListPage } from "@/src/components/pages/product-list";
+
+const SearchPage = async () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <NavMenu />
+      <div className="flex-1">
+        <ProductListPage />
+      </div>
+      <MainFooter />
+    </div>
+  );
+};
+export default SearchPage;
---
+++ web/src/components/pages/product-list.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useProductVariantQuery } from "@/src/hooks/queries/useProductVariants";
+import { MainContainer } from "../containers/main-container";
+import { CategoryNavigation } from "../menus/category-nav";
+import { ProductGrid } from "../products/product-grid";
+import { HighLightTitle } from "../sections/titles/highlight-title";
+import { Subtitle } from "../sections/titles/subtitle";
+import { ProductGridPagination } from "../paginations/product-grid-pagination";
+
+const ProductListPage = () => {
+  const {
+    data: varProducts,
+    isError: varProductsError,
+    isLoading: varProductsLoading,
+  } = useProductVariantQuery(null, {
+    initialData: null,
+  });
+
+  return (
+    <MainContainer>
+      <main className="flex">
+        <aside className="w-1/3">
+          <header className="mr-auto mb-8">
+            <HighLightTitle>Filters</HighLightTitle>
+          </header>
+          <CategoryNavigation />
+        </aside>
+        <section className="w-2/3">
+          <header className="mr-auto mb-8">
+            <HighLightTitle>Products</HighLightTitle>
+            <Subtitle>
+              Explore our products and be plesured with our yummies
+            </Subtitle>
+          </header>
+          {varProductsLoading && <div>Loading...</div>}
+          {varProductsError && <div>Error loading products</div>}
+          <div className="mt-8">
+            {varProducts && <ProductGrid products={varProducts.data} />}
+          </div>
+          <footer className="mt-12">
+            <ProductGridPagination />
+          </footer>
+        </section>
+      </main>
+    </MainContainer>
+  );
+};
+export { ProductListPage };
