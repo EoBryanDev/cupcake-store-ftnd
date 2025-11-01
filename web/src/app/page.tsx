017df4131fbdc9658a410ddb35f66c3e16dcd49b
Bryan-Galaxy-WSL-Ubuntu <eobryandev@gmail.com>
2025-11-01 15:09:02 -0300

    fix: removing get token from server side

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,11 @@
-// page.tsx
 import { MainFooter } from "@/src/components/footers/main-footer";
 import { NavMenu } from "@/src/components/menus/nav-menu";
 import { HomePage } from "@/src/components/pages/home-page";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get(process.env.TOKEN_KEY || "token");
   return (
     <main className="flex min-h-screen max-w-screen flex-col">
-      <NavMenu token={token} />
+      <NavMenu />
       <HomePage />
       <MainFooter />
     </main>
